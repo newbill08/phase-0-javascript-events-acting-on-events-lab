1bac6a26d16add7794d8e2d9230b1b31cafbec79
joshua <josh.newbill08@hotmail.com>
2023-05-05 19:49:38 -0500

    move up and down

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Your code here
+
+
+//Get dodger and change the color
+const dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "#FF69B4";
+
+
+// move position up
+dodger.style.bottom = "0px";
+
+//move left
+//dodger.style.left = "0px";
+
+
+/* 
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+    if (event.key === "ArrowLeft") {
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+    
+        dodger.style.left = `${left - 1}px`;}
+  });
+   */
+  
+
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }else{
+        console.log(e)
+        moveDodgerRight()
+    }
+  });
+  
+
+  function moveDodgerRight() {
+    console.log(dodger.style.left);
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
